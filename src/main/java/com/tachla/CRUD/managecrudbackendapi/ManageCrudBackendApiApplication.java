cccba373c31a306a2c987a0c8dfe00669c961cdb
Your Name <abdellatif.amira@gmail.com>
2023-03-27 13:14:02 +0200

    init project with git

---
+++ src/main/java/com/tachla/CRUD/managecrudbackendapi/ManageCrudBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.tachla.CRUD.managecrudbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageCrudBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageCrudBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tachla/CRUD/managecrudbackendapi/ManageCrudBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tachla.CRUD.managecrudbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageCrudBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
